3fcbfce368dda4e0afbfa39e8df59cfd8a4af91d
Tung-depressedsuperman <110271891+Tung-depressedsuperman@users.noreply.github.com>
2025-03-06 11:15:21 +0700

    Update bootstrap.tf

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,1 +1,6 @@
+module "bootstrap" {
+    source = "git@github.com:Tung-depressedsuperman/aws-bootstrap-new-aws-account/terraform-samples//modules/bootstrap-aws-account"
 
+    state_file_aws_region  = "ap-southeast-2"
+    state_file_bucket_name = "tung-tran-s3-bucket"
+}
